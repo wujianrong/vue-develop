3d1497e6d82d0fa29f6a8b09af35219cf98e8f16
FMS 吴健荣 <611986@ky-tech.com.cn>
2022-06-28 16:37:29 +0800

    feat(all): 完善keyboard

--- src/demo/keyboard/index.vue
+++ src/demo/keyboard/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="keyboard box" v-keyboard:keyboardFn>
+    <div class="keyboard-box" v-keyboard:keyboardFn>
       <!-- 输入任意文本 -->
       <van-field v-model="text" label="文本" />
       <!-- 输入手机号，调起手机号键盘 -->
@@ -7,9 +7,14 @@
       <!-- 允许输入正整数，调起纯数字键盘 -->
       <van-field v-model="digit" type="digit" label="整数" />
       <!-- 允许输入数字，调起带符号的纯数字键盘 -->
-      <van-field v-model="number" type="number" label="数字" />
+      <van-field v-model="number" type="number" label="数字" /> 
+      <van-field v-model="textarea" type="textarea" label="textarea" />
       <!-- 输入密码 -->
       <van-field v-model="password" type="password" label="密码" />
+      <van-radio-group v-model="radio" direction="horizontal" class="radio-group">
+        <van-radio name="1">单选框 1</van-radio>
+        <van-radio name="2">单选框 2</van-radio>
+      </van-radio-group>
     </div>
 </template>
 
@@ -23,7 +28,9 @@ export default {
       tel: '',
       digit: '',
       number: '',
-      password: ''
+      password: '',
+      textarea: '',
+      radio: '1'
     }
   },
   methods: {
@@ -33,3 +40,12 @@ export default {
   }
 }
 </script>
+<style scoped>
+.keyboard-box {
+  background: #ffffff;
+}
+.radio-group {
+  padding: 0 14px;
+  height: 44px;
+}
+</style>
--- src/demo/keyboard/keyboard.js
+++ src/demo/keyboard/keyboard.js
@@ -2,26 +2,38 @@ const ua = navigator.userAgent
 const isAndroid = /(Android);?[\s/]+([\d.]+)?(.*Mobile)/.test(ua)
 
 const originHeight = document.documentElement.clientHeight || document.body.clientHeight
+const originWidth = document.documentElement.clientWidth || document.body.clientWidth
+let isFocus = false
+function callbackHook(cb) {
+  const resizeWeight = document.documentElement.clientWidth || document.body.clientWidth
+  const activeDom = document.activeElement.tagName
+  if(resizeWeight !== originWidth || !['INPUT', 'TEXTAREA'].includes(activeDom)) return isFocus = false
+  cb && cb()
+}
 const keyBoard = {
   inserted(el, binding, vnode) {
     let context = vnode.context
-    console.log({binding})
-    console.log({vnode})
     el.resizeFn = () => {
       const resizeHeight = document.documentElement.clientHeight || document.body.clientHeight
-      if (resizeHeight < originHeight) {
-        context[binding.arg](true)
-      } else {
-        context[binding.arg](false)
-      }
+      callbackHook(()=> {
+        if (resizeHeight < originHeight) {
+          context[binding.arg](true)
+          isFocus = true
+        } else {
+          isFocus = false
+          context[binding.arg](false)
+        }
+      })
     }
     if (isAndroid) {
       window.addEventListener('resize', el.resizeFn)
     }
 
     el.handlerFocusin = () => {
-      el.resizeFn
-      context[binding.arg](true)
+      callbackHook(()=> {
+        isFocus = true
+        context[binding.arg](true)
+      })
       if (isAndroid) {
         setTimeout(() => {
           el.scrollIntoView({
@@ -32,7 +44,7 @@ const keyBoard = {
       }
     }
     el.handlerFocusout = () => {
-      context[binding.arg](false)
+      if(isFocus) context[binding.arg](false)
     }
     el.addEventListener('focusin', el.handlerFocusin)
     el.addEventListener('focusout', el.handlerFocusout)
